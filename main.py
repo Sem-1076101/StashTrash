edce540d479e861a8fe67870df8f856a9b96961f
Sem-1076101 <semjansen.langmuur@gmail.com>
2024-10-14 13:58:24 +0200

    Main.py werkend, login route aangemaakt

--- main.py
+++ main.py
@@ -7,6 +7,10 @@ DATABASEFILE = 'database/database.db'
 def home():
     return render_template('home.html')
 
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
 
 if __name__ == '__main__':
     app.run(debug=True)
